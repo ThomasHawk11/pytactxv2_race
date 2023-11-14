452f0b6fffccca55c919c01d8b47784c10851d7f
plantai211 <plantai211@C304C-212705>
2023-11-14 11:46:29 +0100

    main arbitre conection

--- src/server/main.py
+++ src/server/main.py
@@ -1,11 +1,22 @@
 import api.j2l.pytactx.agent as pytactx
+import os
+from dotenv import load_dotenv
 
-agent = pytactx.Agent(playerId=input("👾 id: "),
-						arena=input("🎲 arena: "),
-						username="demo",
-						password=input("🔑 password: "),
-						server="mqtt.jusdeliens.com",
+load_dotenv()
+ARBITRE = os.getenv("ARBITRE")
+ARENA = os.getenv("ARENA")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+SERVER = os.getenv("SERVER")
+
+agent = pytactx.Agent(playerId=ARBITRE,
+						arena=ARENA,
+						username=USERNAME,
+						password=PASSWORD,
+						server=SERVER,
 						verbosity=2)
+
+
 
 while True:
 	agent.update()
