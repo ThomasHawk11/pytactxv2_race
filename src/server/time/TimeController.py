ed5b491c64f1639da1090ffbda77d1fce81bdbcd
Rom1lsr <93714304+Rom1lsr@users.noreply.github.com>
2023-12-04 15:45:24 +0100

    Update TimeController.py

--- src/server/time/TimeController.py
+++ src/server/time/TimeController.py
@@ -1,130 +1,132 @@
-from typing import Any
 from api.j2l.pytactx.agent import Agent
 
 class TimeController:
 
-  def start(self) -> None:
-    """
-        Start the time master, saving the current timestamp
-        """
+    def start(self) -> None:
+        """Start the time master, saving the current timestamp."""
+        pass
 
-  def getRoundDuration(self) -> int:
-    """
-        Return the round duration
-        """
-    
-  def setRoundDuration(self, timer: int) -> None:
-    """
-        Set the round duration, in seconds
-        """
+    def getRoundDuration(self) -> int:
+        """Return the round duration."""
+        pass
 
-  def getWarmUpDuration(self) -> int:
-    """
-        Return the warm-up duration
-        """
+    def setRoundDuration(self, timer: int) -> None:
+        """Set the round duration, in seconds."""
+        pass
 
-  def setWarmUpDuration(self, timer: int) -> None:
-    """
-        Set the warm-up duration, in seconds
-        """
+    def getWarmUpDuration(self) -> int:
+        """Return the warm-up duration."""
+        pass
 
-  def getLapDuration(self) -> int:
-    """
-        Return the lap duration
-        """
+    def setWarmUpDuration(self, timer: int) -> None:
+        """Set the warm-up duration, in seconds."""
+        pass
 
-  def setLapDuration(self, timer: int) -> None:
-    """
-        Set the lap duration, in seconds
-        """
+    def getLapDuration(self) -> int:
+        """Return the lap duration."""
+        pass
 
-  def startLap(self) -> None:
-    """
-        Start the lap timer, saving the current timestamp
-        """
+    def setLapDuration(self, timer: int) -> None:
+        """Set the lap duration, in seconds."""
+        pass
 
-  def stopLap(self) -> None:
-    """
-        Stop the lap timer
-        """
+    def startLap(self) -> None:
+        """Start the lap timer, saving the current timestamp."""
+        pass
 
-  def getCurrTimestamp(self) -> int:
-    """
-        Return the current timestamp from the server
-        """
+    def stopLap(self) -> None:
+        """Stop the lap timer."""
+        pass
 
-  def getRemainingTime(self) -> int:
-    """
-        Return remaining time based on the startTimestamp and the currTimestamp
-        """
+    def getCurrTimestamp(self) -> int:
+        """Return the current timestamp from the server."""
+        pass
 
-  def setRemainingTime(self) -> None:
-    """
-        Set the remaining time depending on the elapsed time
-        """
+    def getRemainingTime(self) -> int:
+        """Return remaining time based on the startTimestamp and the currTimestamp."""
+        pass
+
+    def setRemainingTime(self) -> None:
+        """Set the remaining time depending on the elapsed time."""
+        pass
 
 class TimeController(TimeController):
-  def __init__(self,
-               agent: Agent,
-               round_duration: int = 300,
-               warm_up_duration: int = 60,
-               lap_duration: int = 30) -> None:
-    self.__pytactxAgent = agent
-    self.__roundDuration = round_duration
-    self.__warmUpDuration = warm_up_duration
-    self.__lapDuration = lap_duration
-    self.__startTimestamp = None
-    self.__startLapTimestamp = None
-    self.__remainingTime = None
-    self.__remainingLapTime = None
+    def __init__(self,
+                 agent: Agent,
+                 round_duration: int = 300,
+                 warm_up_duration: int = 60,
+                 lap_duration: int = 30) -> None:
+        """
+        Initialize TimeController instance.
 
-  def start(self) -> None:
-    self.__startTimestamp = self.__pytactxAgent.game["t"]
+        :param agent: The game agent.
+        :param round_duration: The duration of a round in seconds.
+        :param warm_up_duration: The duration of warm-up in seconds.
+        :param lap_duration: The duration of a lap in seconds.
+        """
+        self.__pytactx_agent = agent
+        self.__round_duration = round_duration
+        self.__warm_up_duration = warm_up_duration
+        self.__lap_duration = lap_duration
+        self.__start_timestamp = None
+        self.__start_lap_timestamp = None
+        self.__remaining_time = None
+        self.__remaining_lap_time = None
+        self.__best_lap_time = float('inf')  # Initialise avec un positif infini
 
-  def getRoundDuration(self) -> int:
-    return self.__roundDuration
+    def start(self) -> None:
+        """Start the time master, saving the current timestamp."""
+        self.__start_timestamp = self.__pytactx_agent.game["t"]
 
-  def setRoundDuration(self, timer: int) -> Any:
-    if timer < 0:
-      return "Il faut un entier positif"
-    self.__roundDuration = timer
-    return self.__roundDuration
+    def startLap(self) -> None:
+        """Start the lap timer, saving the current timestamp."""
+        self.__start_lap_timestamp = self.__pytactx_agent.game["t"]
 
-  def getWarmUpDuration(self) -> int:
-    return self.__warmUpDuration
+    def stopLap(self) -> None:
+        """Stop the lap timer and update the best lap time if needed."""
+        elapsed_time = self.__pytactx_agent.game["t"] - self.__start_lap_timestamp
+        self.__remaining_lap_time = elapsed_time
+        if elapsed_time < self.__best_lap_time:
+            self.__best_lap_time = elapsed_time
 
-  def setWarmUpDuration(self, timer: int) -> Any:
-    if timer < 0:
-      return "Il faut un entier positif"
-    self.__warmUpDuration = timer
-    return self.__warmUpDuration
+    def getBestLapTime(self) -> int:
+        """Return the best lap time."""
+        return self.__best_lap_time
 
-  def getLapDuration(self) -> int:
-    return self.__lapDuration
+    def getRoundDuration(self) -> int:
+        return self.__round_duration
 
-  def setLapDuration(self, timer: int) -> Any:
-    if timer < 0:
-      return "Il faut un entier positif"
-    self.__lapDuration = timer
-    return self.__lapDuration
+    def setRoundDuration(self, timer: int) -> None:
+        if timer < 0:
+            return "Il faut un entier positif"
+        self.__round_duration = timer
 
-  def startLap(self) -> None:
-    self.__startLapTimestamp = self.__pytactxAgent.game["t"]
+    def getWarmUpDuration(self) -> int:
+        return self.__warm_up_duration
 
-  def stopLap(self) -> None:
-    self.__remainingLapTime = self.__pytactxAgent.game[
-      "t"] - self.__startLapTimestamp
+    def setWarmUpDuration(self, timer: int) -> None:
+        if timer < 0:
+            return "Il faut un entier positif"
+        self.__warm_up_duration = timer
 
-  def getCurrTimestamp(self) -> int:
-    return self.__pytactxAgent.game["t"]
+    def getLapDuration(self) -> int:
+        return self.__lap_duration
 
-  def getRemainingTime(self) -> int:
-    self.setRemainingTime()
-    return self.__remainingTime
+    def setLapDuration(self, timer: int) -> None:
+        if timer < 0:
+            return "Il faut un entier positif"
+        self.__lap_duration = timer
 
-  def setRemainingTime(self) -> None:
-    delta_time = (self.getCurrTimestamp() - self.__startTimestamp) // 1000
-    self.__remainingTime = max(0, self.__roundDuration - delta_time)
+    def getCurrTimestamp(self) -> int:
+        return self.__pytactx_agent.game["t"]
 
-  def getRemainingLapTime(self) -> int:
-    return self.__lapDuration - (self.__remainingLapTime // 1000)
+    def getRemainingTime(self) -> int:
+        self.setRemainingTime()
+        return self.__remaining_time
+
+    def setRemainingTime(self) -> None:
+        delta_time = (self.getCurrTimestamp() - self.__start_timestamp) / 1000
+        self.__remaining_time = max(0, self.__round_duration - delta_time)
+
+    def getRemainingLapTime(self) -> int:
+        return self.__lap_duration - (self.__remaining_lap_time / 1000)
