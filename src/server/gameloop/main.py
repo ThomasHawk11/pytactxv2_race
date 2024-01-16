1ad6aa49317d3970b49400f983c1e0ddb6153cb6
Mylan MEGARD <115895869+stryax1@users.noreply.github.com>
2024-01-16 13:55:46 +0100

    TurboVroum Latest version

--- src/server/gameloop/main.py
+++ src/server/gameloop/main.py
@@ -1,14 +1,18 @@
+import os
+from dotenv import load_dotenv
 import gameLoop
 import time
 import j2l.pytactx.agent as pytactx
 
+load_dotenv()
+
 # Create the agent
-agent = pytactx.Agent(playerId=input("gameloop manager login : "),
-                      arena="turbovroum",
-                      username="demo",
-                      password=input("password : ",
-                      server="mqtt.jusdeliens.com",
-                      verbosity=2)
+agent = pytactx.Agent(playerId=os.getenv("PLAYERID"),
+	arena=os.getenv("ARENA"),
+	username=os.getenv("USERNAME"),
+	password=os.getenv("PASSWORD"),
+	server=os.getenv("SERVER"),
+	verbosity=2)
 
 # The gameloop
 cls = gameLoop.GameLoop(agent)
