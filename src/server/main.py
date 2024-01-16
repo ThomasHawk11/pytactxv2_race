bc3f4162e80fe87f8d841f9c4f9a83288e694d01
Mathieu ORDONNAUD <94300401+CubiBox@users.noreply.github.com>
2024-01-16 11:00:49 +0100

    Update main.py

--- src/server/main.py
+++ src/server/main.py
@@ -1,4 +1,9 @@
 import field/fieldController.py
 import vehicles/vehiclesController.py
 import time/TimeController.py
+
+import mapManager/mapManager.py
+import rulesManager/rulesManager.py
+
+import mapManager/main.py
 import gameloop/main.py
